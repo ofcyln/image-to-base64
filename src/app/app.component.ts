c694739f76865323fbdfdfa708f0ea10b94ff496
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-07-25 22:06:18 +0200

    Alert service optimisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertService } from './core/alert/alert.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Image to Base64';
 
-    constructor(public alertService: AlertService) {}
-
-    ngOnInit(): void {
-        setTimeout(() => this.alertService.error('asd'), 1000);
-    }
+    constructor() {}
 }
