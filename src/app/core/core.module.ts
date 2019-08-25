f896a9e75348a50d021000846a3b23749fd58d3e
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-08-25 22:15:30 +0200

    #1 Uploader plugin integration within app

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,10 +9,11 @@ import { AlertModule } from './alert/alert.module';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NgxUploaderModule } from 'ngx-uploader';
 
 @NgModule({
     declarations: [HeaderComponent, NotFoundComponent, FooterComponent],
-    imports: [CommonModule, AlertModule, AppRoutingModule, BrowserAnimationsModule, DragDropModule],
-    exports: [HeaderComponent, AlertModule, AppRoutingModule, FooterComponent, NotFoundComponent],
+    imports: [CommonModule, AlertModule, AppRoutingModule, BrowserAnimationsModule, DragDropModule, NgxUploaderModule],
+    exports: [HeaderComponent, AlertModule, AppRoutingModule, FooterComponent, NotFoundComponent, NgxUploaderModule],
 })
 export class CoreModule {}
