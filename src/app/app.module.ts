73edda83ac1989feaf94a4c157d6616f161000f2
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-08-28 22:15:50 +0200

    #1 Upload by drag-drop and native button integration has been made, base output area UI design integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -7,7 +8,7 @@ import { ConverterComponent } from './components/converter/converter.component';
 
 @NgModule({
     declarations: [AppComponent, ConverterComponent],
-    imports: [BrowserModule, CoreModule],
+    imports: [BrowserModule, CoreModule, ReactiveFormsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-converter',
     templateUrl: './converter.component.html',
     styleUrls: ['./converter.component.scss'],
 })
-export class ConverterComponent {
+export class ConverterComponent implements OnInit {
     public droppedFiles: File[] = [];
     public uploadedFiles: File[] = [];
     public imgURL: any;
     public imageSrc: string | ArrayBuffer;
+    public textAreaForm: FormGroup;
 
     constructor() {}
+
+    ngOnInit(): void {
+        this.textAreaForm = new FormGroup({
+            droppedURI: new FormControl('Loading...'),
+            uploadedURI: new FormControl('Loading...'),
+            uploadedImgTagName: new FormControl('Loading...'),
+            droppedImgTagName: new FormControl('Loading...'),
+            uploadedBackgroundStyle: new FormControl('Loading...'),
+            droppedBackgroundStyle: new FormControl('Loading...'),
+        });
+    }
 
     onDropHandler(droppedFiles: Array<File>) {
         this.droppedFiles = [];
         this.uploadedFiles = [];
+        this.textAreaForm.controls['droppedURI'].setValue('Loading...');
+        this.textAreaForm.controls['droppedImgTagName'].setValue('Loading...');
+        this.textAreaForm.controls['droppedBackgroundStyle'].setValue('Loading...');
 
         let reader = new FileReader();
 
@@ -23,20 +39,29 @@ export class ConverterComponent {
 
         const droppedFile = droppedFiles[0];
 
-        console.log('dropped file', droppedFile);
-
-        reader.readAsDataURL(droppedFiles[0]);
+        reader.readAsDataURL(droppedFile);
 
         reader.onload = (event) => {
             this.imageSrc = reader.result;
 
-            console.log('dropped reader onload result', this.imageSrc);
+            this.textAreaForm.controls['droppedURI'].setValue(this.imageSrc);
+
+            this.textAreaForm.controls['droppedImgTagName'].setValue(
+                '<img alt="' + droppedFile.name + '" src="' + this.imageSrc + '"/>',
+            );
+
+            this.textAreaForm.controls['droppedBackgroundStyle'].setValue(
+                'background-image: url("' + this.imageSrc + '");',
+            );
         };
     }
 
     onUploadByButton(uploadedFiles: FileList) {
         this.uploadedFiles = [];
         this.droppedFiles = [];
+        this.textAreaForm.controls['uploadedURI'].setValue('Loading...');
+        this.textAreaForm.controls['uploadedImgTagName'].setValue('Loading...');
+        this.textAreaForm.controls['uploadedBackgroundStyle'].setValue('Loading...');
 
         let reader = new FileReader();
 
@@ -44,14 +69,20 @@ export class ConverterComponent {
 
         this.uploadedFiles.push(uploadedFile);
 
-        console.log('uploaded file', uploadedFile);
-
-        reader.readAsDataURL(uploadedFiles[0]);
+        reader.readAsDataURL(uploadedFile);
 
         reader.onload = (event) => {
             this.imgURL = reader.result;
 
-            console.log('uploaded file reader onload result', this.imgURL);
+            this.textAreaForm.controls['uploadedURI'].setValue(this.imgURL);
+
+            this.textAreaForm.controls['uploadedImgTagName'].setValue(
+                '<img alt="' + uploadedFile.name + '" src="' + this.imgURL + '"/>',
+            );
+
+            this.textAreaForm.controls['uploadedBackgroundStyle'].setValue(
+                'background-image: url("' + this.imgURL + '");',
+            );
         };
     }
 }
