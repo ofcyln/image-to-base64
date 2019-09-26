17eb5ad29e585cf990198e56f2d1f0978bd84521
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-09-26 21:55:51 +0200

    Base PWA files added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ConverterComponent } from './components/converter/converter.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent, ConverterComponent],
-    imports: [BrowserModule, CoreModule, ReactiveFormsModule],
+    imports: [
+        BrowserModule,
+        CoreModule,
+        ReactiveFormsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
