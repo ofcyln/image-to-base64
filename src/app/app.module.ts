d8d20264150d0490733196378427c1f056aa60dc
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-12-18 21:55:50 +0100

    package integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { CoreModule } from './core/core.module';
+
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
+import { NgxUploaderModule } from 'ngx-uploader';
+
 import { ConverterComponent } from './components/converter/converter.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -13,6 +18,7 @@ import { environment } from '../environments/environment';
     imports: [
         BrowserModule,
         CoreModule,
+        NgxUploaderModule,
         ReactiveFormsModule,
         ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ],
